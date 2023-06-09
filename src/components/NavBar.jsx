d3c0c8e5fda417cf0413853dbfa08e32dbec8c8d
PiyoPiyo4 <pio_giovanni@yahoo.com>
2023-06-09 12:24:51 +1000

    navbar position and mobile responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -25,7 +25,7 @@ function Navbar(props) {
   // const classes = useStyles(props);
 
   return (  
-    <TransparentAppBar position="fixed" >
+    <TransparentAppBar position="absolute" >
       <Toolbar>
         {/* <Link to ='/' style={{ flexGrow: 1 }} > */}
         <div style={{ flexGrow: 1 }} >
