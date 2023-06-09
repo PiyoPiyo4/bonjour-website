95caf2ea67b8bfd00dc40a713d82bb828df11e8e
PiyoPiyo4 <pio_giovanni@yahoo.com>
2023-06-09 10:45:54 +1000

    stable change

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { AppBar, Toolbar, Button, styled } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 // import { makeStyles } from '@mui/styles';
 import { Link } from 'react-router-dom';
 import { SocialIcon } from 'react-social-icons';
@@ -20,16 +21,22 @@ const TransparentAppBar = styled(AppBar)(({ theme }) => ({
 // }));
 
 function Navbar(props) {
+  const navigate = useNavigate();
   // const classes = useStyles(props);
 
   return (  
     <TransparentAppBar position="fixed">
       <Toolbar>
-        <Link to ='/' style={{ flexGrow: 1 }} >
-          <img src={logo} style={{ height: '40px'}} alt='LogoBonjour' />
-        </Link>
+        {/* <Link to ='/' style={{ flexGrow: 1 }} > */}
+        <div style={{ flexGrow: 1 }} >
+          <img src={logo} style={{ height: '40px', cursor: 'pointer' }} alt='LogoBonjour' onClick={() => navigate('/')}/>
+        </div>
+        {/* </Link> */}
         <SocialIcon url="https://www.instagram.com/bonjour.ent/" fgColor={props.color} bgColor="transparent" target="_blank" rel="noopener noreferrer"/>
-        <Button component={Link} to="/YODMG" color="primary"  style={{ fontFamily: 'HFBronte', color: props.color, fontWeight: 'bold', fontSize: '20px' }}>Artists</Button>
+        <Button color="primary" 
+          style={{ fontFamily: 'HFBronte', color: props.color, fontWeight: 'bold', fontSize: '20px' }}
+          onClick={() => navigate('/YODMG')}  
+        >Artists</Button>
         {/* <Button component={Link} to="/about" color="primary" style={{ fontFamily: 'HFBronte', color: props.color, fontWeight: 'bold', fontSize: '20px' }}>About</Button> */}
         {/* <Button component={Link} to="/contact" color="primary" style={{ fontFamily: 'HFBronte', color: props.color, fontWeight: 'bold' }}>Contact</Button> */}
       </Toolbar>
