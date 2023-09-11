d27ea87b982a9b8c5a846f93fb89abc8b10ab7fb
PiyoPiyo4 <pio_giovanni@yahoo.com>
2023-09-11 21:36:00 +1000

    singles and albums tag

--- src/components/ArtistSongsList.jsx
+++ src/components/ArtistSongsList.jsx
@@ -4,30 +4,31 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSpotify, faYoutube, faApple} from '@fortawesome/free-brands-svg-icons'
 import './styles/ArtistSongsListCSS.css'
 
-const AristSongsList = ({cards}) => {
+const AristSongsList = ({cards, type}) => {
   const icons = [faSpotify, faApple, faYoutube, faYoutube]
-  console.log(cards)
   return(<>
   <div id="cardContain">
     <CardGroup id="cardGroup" >
-      {cards.map((card) => (
-        <Card key={card.title} id='cardItem'>
-          <Card.Img src={card.image} id='cardImage' />
-          <Card.Body>
-            <Card.Title id='cardTitle'>{card.title}</Card.Title>
-              <Dropdown id='mainDropdown' drop='down-centered'>
-                <Dropdown.Toggle variant="success" id="dropdownToggle" color='#d1aead'> Streams </Dropdown.Toggle>
-                <Dropdown.Menu id="dropdownMenu" >
-                  {card.links.map((link, i) => (
-                    <Dropdown.Item key={link.text} href={link.url} target="_blank">
-                      <FontAwesomeIcon icon={icons[i]} fontSize='25px' fixedWidth /> {link.text}
-                    </Dropdown.Item>
-                  ))}
-                </Dropdown.Menu>
-              </Dropdown>
-          </Card.Body>
-        </Card>
-      ))}
+      {cards.map((card) => {
+        return card.text === type ?
+          (<Card key={card.title} id='cardItem'>
+            <Card.Img src={card.image} id='cardImage' />
+            <Card.Body>
+              <Card.Title id='cardTitle'>{card.title}</Card.Title>
+                <Dropdown id='mainDropdown' drop='down-centered'>
+                  <Dropdown.Toggle variant="success" id="dropdownToggle" color='#d1aead'> Streams </Dropdown.Toggle>
+                  <Dropdown.Menu id="dropdownMenu" >
+                    {card.links.map((link, i) => (
+                      <Dropdown.Item key={link.text} href={link.url} target="_blank">
+                        <FontAwesomeIcon icon={icons[i]} fontSize='25px' fixedWidth /> {link.text}
+                      </Dropdown.Item>
+                    ))}
+                  </Dropdown.Menu>
+                </Dropdown>
+            </Card.Body>
+          </Card>)
+          : null
+      })}
     </CardGroup>
   </div>
   </>)
